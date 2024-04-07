bd24bcde64919e035eefd28ccc3b012435d8a869
OdakK <julien.2002@yahoo.fr>
2024-04-07 18:02:01 +0200

    devops: Create base project

---
+++ src/test/java/com/immobyletteapimain/ImmobyletteApiMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immobyletteapimain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmobyletteApiMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

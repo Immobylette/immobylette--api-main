8236014e7f314976b9cddaecd0a42e9ea42b28d7
Romain Lefebvre <romain.lefebvre1104@gmail.com>
2024-05-23 16:55:47 +0200

    [UPDATE] use security filter chain to auth

---
+++ src/main/java/com/immobylette/api/main/security/AuthenticationFilter.java
@@ -0,0 +1,31 @@
+package com.immobylette.api.main.security;
+
+import com.immobylette.api.main.config.AuthConfig;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+@Order(1)
+public class AuthenticationFilter extends OncePerRequestFilter {
+    private final AuthConfig authConfig;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("AuthenticationFilter");
+        String apiKey = request.getHeader("X-Api-Key");
+        if (apiKey == null || apiKey.isEmpty() || !apiKey.equals(authConfig.getApiKey())){
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Denied");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
